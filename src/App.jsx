53963e34d3577afcf8aa095cf5caeece9701f4e1
tamim <abutalhatamim017@gmail.com>
2024-12-08 01:39:52 +0600

    Hero section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import Home from './pages/Home'
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
      
-      
+      <Home/>
       
       
     </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import Hero from '../components/hero';
-import Categorysection from './Categorysection';
+import Hero from './../components/Hero';
+import Categorysection from './Categorysection'; 
+
 
 const Home = () => {
     return (
